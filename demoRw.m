1d564a3a3e9bbe543f42c1b1dad2854b37616ff8
tcyhx <yuanhx@mail.ustc.edu.cn>
2014-05-21 23:25:49 -0400

    xxxx

---
+++ demoRw.m
@@ -0,0 +1,30 @@
+%Semi-automatic 2d-to-3d conversion using random walks
+%04/27/14 - Hongxing Yuan
+%
+%The code  has been tested in MATLAB R2013a running on Windows 7(64bit). 
+%- Note:
+%(1) If you plan to use this software or any variant of it, please, refer the following paper:
+%    H. X. Yuan, S. Q. Wu, Nonlocal Random Walks for Semi-Automatic 2D-to-3D Image Conversion, 
+%    submitted to IEEE Transactions on Signal Processing
+%(2) For any questions, suggestions or bug reports, please, contact yuanhx@mail.ustc.edu.cn
+
+clear
+close all
+addpath('algorithm');
+
+dirname  = 'imgs/girl';
+filename = 'girl';
+savename = 'depth_rw';
+betaC    = 60;                                                 %The free paremeter controling how color dissimilarity
+
+%Read image
+img   = imread(sprintf('%s/%s.png',dirname,filename));         %read 2D color image
+lbls  = double(imread(sprintf('%s/strokes.png',dirname)))/255; %read user labels and normalize them to [0,1]
+mask  = imread(sprintf('%s/mask.png',dirname));                %read user mask
+mask  = im2bw(mask,0);
+seeds = find(mask==1);                                         %get seeded pixels
+%main routine
+prob  = rwCore(img, seeds, lbls, betaC);                       %estimate probability of each pixel which has the depth value of 255
+depth = uint8(prob.*255);                                      %mapping probabilites to depth values
+imshow(depth);
+imwrite(depth,sprintf('%s/%s.png',dirname,savename));          %save results
---
+++ readme.txt
@@ -0,0 +1,15 @@
+- Author: Hongxing Yuan (yuanhx@mail.ustc.edu.cn)
+- Version: 1.0 (last update: May-21-2014)
+
+- Note:
+(1) The code is based on our submitted paper:
+    Nonlocal Random Walks for Semi-Automatic 2D-to-3D Image Conversion, submitted to IEEE Transactions on Signal Processing
+(2) Some code is in p-file format. All the code will be shared when the review of our paper is finished.
+(4) The code has been tested in MATLAB R2013a running on Windows 7(64bit). 
+    
+- Usage example:
+(1) run demoRw to do semi-automatic 2D-to-3D conversion using RW algorithm.
+(2) run demoRwGc to perform semi-automatic 2D-to-3D conversion using hybrid GC and RW algorithm.
+(3) run demoRwNonlocal to do semi-automatic 2D-to-3D conversion using our method.
+    
+- For any questions, suggestions or bug reports, please, contact yuanhx@mail.ustc.edu.cn
